de8570cd93e177fd5b3f8dc6ee4cc28c18855536
ANDRE SHONUBI <andre@codedre.com>
2016-01-14 16:49:46 -0500

    setting site template

--- public/components/sidePanel/sidePanel.directive.js
+++ public/components/sidePanel/sidePanel.directive.js
@@ -8,6 +8,14 @@
         templateUrl: 'components/sidePanel/sidePanel.html',
         restrict: 'E',
         link: function (scope, element, attrs) {
+          scope.toggleMenu = function() {
+            $('.button-collapse').sideNav({
+                menuWidth: 300, // Default is 240
+                edge: 'right', // Choose the horizontal origin
+                closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+              }
+            );
+          };
         }
       };
     });
